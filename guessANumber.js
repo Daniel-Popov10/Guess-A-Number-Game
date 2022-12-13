50fb7c03c1f86cdbf9016a1acbf0e87ebcd50f9b
Daniel Popov <d.popov0470@gmail.com>
2022-12-13 21:34:11 +0200

    Update guessANumber.js
    Added a feature where the player has a limited amount of attempts to guess the number.

--- guessANumber.js
+++ guessANumber.js
@@ -6,10 +6,12 @@ function guessANumber() {
 
     let computerGuess = Math.floor(Math.random() * 100);
     let guess;
+    let maxTries = 0;
 
     let recursiveAsyncReadLine = function () {
         readline.question('Guess the number(0-100): ', number => {
             guess = Number(number);
+
 
             if (guess <= 100 && guess >= 0) {
                 if (guess == computerGuess) {
@@ -17,14 +19,22 @@ function guessANumber() {
                     return readline.close();
                 } else if (guess < computerGuess) {
                     console.log('Too low!');
+                    maxTries++;
                     recursiveAsyncReadLine()
                 } else if (guess > computerGuess) {
                     console.log('Too high!');
+                    maxTries++;
                     recursiveAsyncReadLine()
                 }
             } else {
                 console.log('Invalid input! Try again....');
+                maxTries++;
                 recursiveAsyncReadLine();
+            }
+
+            if (maxTries >= 10) {
+                console.log('Exceeded the maximum attempts... Game Over!');
+                return readline.close();
             }
         });
     }
